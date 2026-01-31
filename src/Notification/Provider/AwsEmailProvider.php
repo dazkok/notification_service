5e063c06fbca16110b8969a0d2c9c704d2f8cf86
Pavlo Vovk <dazkok@gmail.com>
2026-01-31 18:38:18 +0100

    Basic working AWS SES

---
+++ src/Notification/Provider/AwsEmailProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notification\Provider;
+
+use App\Enum\NotificationChannel;
+use App\Notification\NotificationProviderInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+readonly class AwsEmailProvider implements NotificationProviderInterface
+{
+    /**
+     * @param MailerInterface $mailer
+     * @param string $fromEmail
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        private MailerInterface $mailer,
+        private string          $fromEmail,
+        private LoggerInterface $logger
+    )
+    {
+    }
+
+    /**
+     * @param array $content
+     * @return bool
+     * @throws TransportExceptionInterface
+     */
+    public function send(array $content): bool
+    {
+        try {
+            $email = new Email()
+                ->from($this->fromEmail)
+                ->to($content['email'])
+                ->subject($content['subject'] ?? 'Notification')
+                ->text($content['body'] ?? '');
+
+            $this->mailer->send($email);
+
+            return true;
+        } catch (TransportExceptionInterface $e) {
+            $this->logger->error('AWS SES Failed: ' . $e->getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * @param string $channel
+     * @return bool
+     */
+    public function supports(string $channel): bool
+    {
+        return $channel === NotificationChannel::EMAIL->value;
+    }
+}
--- src/Notification/Provider/FakeSmsProvider.php
+++ src/Notification/Provider/FakeSmsProvider.php
@@ -5,13 +5,13 @@ namespace App\Notification\Provider;
 use App\Enum\NotificationChannel;
 use App\Notification\NotificationProviderInterface;
 
-class TwilioProvider implements NotificationProviderInterface
+class FakeSmsProvider implements NotificationProviderInterface
 {
     public function send(array $content): bool
     {
-        // Twilio send logic
+        // always return success (fake)
 
-        return false;
+        return true;
     }
 
     public function supports(string $channel): bool
